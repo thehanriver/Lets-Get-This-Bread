0fd15fd512e9c9610ec6708422cfd341a7327855
loxaedward626 <eddie@dhcp-wifi-8021x-168-122-216-234.bu.edu>
2018-12-09 21:25:23 -0500

    Revert "Merge branch 'motionControl' into Layout"
    This reverts commit b9dd6f80ab4d3e4eedfd19175fb68fa63233f547, reversing
    changes made to abd2f4b379e5ce3411d0ea4057b87130f2229e8d.

--- app/src/main/java/com/example/eddie/letsgetthisbread/SettingsScreen.java
+++ app/src/main/java/com/example/eddie/letsgetthisbread/SettingsScreen.java
@@ -1,23 +1,16 @@
 package com.example.eddie.letsgetthisbread;
 
-import android.content.Context;
-import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.content.Intent;
-import android.view.View;
 
 public class SettingsScreen extends AppCompatActivity {
-
-    SharedPreferences control_data;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings_screen);
-
-        control_data = getSharedPreferences("GAME_DATA" , Context.MODE_PRIVATE);
     }
 
     // Disable return
@@ -33,19 +26,11 @@ public class SettingsScreen extends AppCompatActivity {
         return super.dispatchKeyEvent(event);
     }
 
-    public void motion(View view) {
-        SharedPreferences.Editor editor = control_data.edit();
-        editor.putBoolean("GAME_DATA" , true);
-        editor.commit();
+    public void motion() {
 
     }
 
-    public void button(View view) {
-        SharedPreferences.Editor editor = control_data.edit();
-        editor.putBoolean("GAME_DATA" , false);
-        editor.commit();
+    public void button() {
+
     }
-    public void mainMenu(View view){
-            startActivity(new Intent(getApplicationContext(), StartScreen.class));
-        }
 }
